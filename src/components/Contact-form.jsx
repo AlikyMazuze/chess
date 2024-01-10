27f004bc7daeb0c2b4c198346515c216264e3b5d
AlikyMazuze <106402062+AlikyMazuze@users.noreply.github.com>
2024-01-10 20:36:52 +0200

    adding submit button to contact form

--- src/components/Contact-form.jsx
+++ src/components/Contact-form.jsx
@@ -22,6 +22,7 @@ function ContactForm() {
             <input id=":S3:" autocomplete="organization" placeholder=" " class="peer block w-full border border-neutral-300 bg-transparent px-6 pb-4 pt-12 text-base/6 text-neutral-950 ring-4 ring-transparent transition focus:border-neutral-950 focus:outline-none focus:ring-neutral-950/5 group-first:rounded-t-2xl group-last:rounded-b-2xl" type="text" name="message"/>
             <label htmlFor=":S3:" class="pointer-events-none absolute left-6 top-1/2 -mt-3 origin-left text-base/6 text-neutral-500 transition-all duration-200 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:font-semibold peer-focus:text-neutral-950 peer-[:not(:placeholder-shown)]:-translate-y-4 peer-[:not(:placeholder-shown)]:scale-75 peer-[:not(:placeholder-shown)]:font-semibold peer-[:not(:placeholder-shown)]:text-neutral-950">Mensagem</label>
         </div>  
+        <button class="mt-10 inline-flex rounded-full px-4 py-1.5 text-sm font-semibold transition bg-neutral-950 text-white hover:bg-neutral-800" type="submit"><span class="relative top-px">Submeter mensagem</span></button>
     </form>  
     )
 }
